e5990ed33bb80bf4f1d58332a42d6ff9d9565fc2
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2019-02-16 16:48:22 -0200

    Colocando Um Site Que Pode Ajudar Nos Desenhos Sobre As Fotos!

---
+++ Drawing/www.walterzorn.de/en/scripts/wz_grapherstr.js
@@ -0,0 +1,42 @@
+if(window.G_LANG && G_LANG == "D")
+{
+    var S_SHOWGRID = 'Gitter zeigen';
+    var S_SAMESCALE = 'ident. Ma&szlig;stab f&uuml;r beide Achsen [*]';
+    var S_FITWND = 'Diagramm- Gr&ouml;&szlig;e an Fenster anpassen [*]';
+    var S_PRTVIEW = 'Druckansicht';
+    var S_POSTREPING = '[*] sichtbar erst nach Neuzeichnen';
+    var S_COORDTIP = 'Werte im Diagramm k&ouml;nnen mit der Maus n&auml;herungsweise abgetastet werden';
+    var S_DELGRAPHS = 'Sollen alle Graphen aus der Zeichenfläche gelöscht werden?';
+    var S_DELLASTGRAPH = 'Soll der letzte Graph wirklich gelöscht werden?';
+    var S_OLDBROWSER = 'Die JavaScript-Unterstuetzung Ihres Browsers ist unzureichend';
+    var S_ERR_BASE = 'Ungültige Eingabe für ';
+    var S_ERR_XRANGE = 'Definitionsbereich von x.\nx min muss kleiner als x max sein.';
+    var S_ERR_YRANGE = 'Wertebereich von y.\ny min muss kleiner als y max sein.';
+    var S_ERR_TERM = 'Funktionsterm.\nKein Tippfehler?\nNur erlaubte Rechenoperatoren verwendet?\nKlammerung ok?\nBitte Syntax überprüfen.';
+    var S_ERR_TCOMM = 'Funktionsterm.\nZu viele Kommata.';
+    var S_ERR_TDECI = 'Funktionsterm.\nZu viele Kommata.\nBitte Punkt als Dezimaltrennzeichen verwenden.';
+    var S_ERR_TMISSCOMM = 'Funktionsterm.\n(Mindestens) eine der Hilfsfunktionen benötigt zwei (gaussd() drei) Argumente.\nAnscheinend fehlt ein Argument, bzw. das Komma zwischen den Argumenten.';
+    var S_ERR_TPIPE = 'Funktionsterm.\nEin senkrechter Strich (Pipe) zuviel oder zuwenig.';
+    var S_ERR_TPARENTH = 'Funktionsterm.\nFalsche Klammerung.\nAnzahl öffnender und schließender Klammern stimmt nicht überein.';
+}
+else
+{
+    var S_SHOWGRID = 'Show grid lines';
+    var S_SAMESCALE = 'Same scale for both axes [*]';
+    var S_FITWND = 'Fit diagram into window [*]';
+    var S_PRTVIEW = 'Print preview';
+    var S_POSTREPING = '[*] visible after redrawing';
+    var S_COORDTIP = 'Move mouse over diagram to scan coordinates';
+    var S_DELGRAPHS = 'Delete all graphs?';
+    var S_DELLASTGRAPH = 'Delete latest graph?';
+    var S_OLDBROWSER = 'Your browser\'s JavaScript implementation is insufficient.';
+    var S_ERR_BASE = 'Invalid ';
+    var S_ERR_XRANGE = 'interval specified for x.\nx min must be lesser than x max.';
+    var S_ERR_YRANGE = 'range for y.\ny min must be lesser than y max.';
+    var S_ERR_TERM = 'function expression.\nTypo?\nInvalid usage of operators?\nError with opening and closing parentheses?\nPlease check syntax.';
+    var S_ERR_TCOMM = 'function expression.\nToo many commas.';
+    var S_ERR_TDECI = 'function expression.\nToo many commas.\nUse point as decimal point.';
+    var S_ERR_TMISSCOMM = 'function expression.\n(At least) one of the helper functions requires two (or gaussd(): three) arguments.\nApparently one argument is missing, or a comma separating arguments from each other.';
+    var S_ERR_TPIPE = 'function expression.\nApparently typo. One too many pipes (\'|\').';
+    var S_ERR_TPARENTH = 'function expression.\nOpening and closing parentheses don\'t match.';
+}
